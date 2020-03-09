1805491eb004902d28498e8b23740e90a415de3e
jalvarez-outburst <38958235+jalvarez-outburst@users.noreply.github.com>
2020-03-09 08:50:29 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -17,7 +17,3 @@ def home():
         "result": _data
     }
     return render_template("pages/results.html", locals=_template_vars)
-
-if __name__ == "__main__":
-    # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=False, port=80)
